7e4523a83f6d0a0abab57c303c2ac4876d62dc30
aubonee <chytahsin.2210@gmail.com>
2023-12-21 00:11:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port =process.env.PORT || 5000;
+
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log(`task management is Running on port ${port}`);
+  });
