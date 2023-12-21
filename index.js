dbd2a2e5f932730414d26f5bdc299323bb58faf5
aubonee <chytahsin.2210@gmail.com>
2023-12-21 17:12:10 +0600

    basic setup done

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 const port =process.env.PORT || 5000;
-
-
+//userAubonee
+//l1GGRRzpDqhkTXLb
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world')
