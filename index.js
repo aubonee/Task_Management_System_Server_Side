d109a1cb2de9c757e010483e2adc40e088fa30dd
aubonee <chytahsin.2210@gmail.com>
2023-12-22 11:57:17 +0600

    delete api added

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ async function run() {
       res.send(result);
   })
 
+  app.delete('/alltask/:id', async(req,res) =>  {
+    const id=req.params.id;
+      const query ={ _id: new ObjectId(id)};
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+  })
+
 
   } finally {
  
