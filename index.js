b4435973563274f203df0ce84beb6c7f05e0d8c5
aubonee <chytahsin.2210@gmail.com>
2023-12-22 10:09:14 +0600

    task data posted successfully

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app =express();
+require('dotenv').config()
+
+const cors = require('cors');
+
+
 
 const port =process.env.PORT || 5000;
-//userAubonee
-//l1GGRRzpDqhkTXLb
-// respond with "hello world" when a GET request is made to the homepage
+//middleware
+
+app.use(cors({origin:[ 'http://localhost:5173',
+]}));
+app.use(express.json());
+
+    
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rns2r0a.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+      
+    const taskCollection=client.db('TaskPlannerDb').collection('Alltasks');
+    
+    app.post('/alltask', async(req,res)=>{
+      const user =req.body;
+      const result =await taskCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+
+  } finally {
+ 
+  }
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
